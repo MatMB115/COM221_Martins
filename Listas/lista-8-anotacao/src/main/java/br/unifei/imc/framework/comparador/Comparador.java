90b47ef2f14c432735c4212609c055376379c449
MatMB115 <MatMB115@users.noreply.github.com>
2022-11-22 22:12:24 -0300

    lista 8 corrigindo nomes

--- Listas/lista-8-anotacao/src/main/java/br/unifei/imc/framework/comparador/Comparador.java
+++ Listas/lista-8-anotacao/src/main/java/br/unifei/imc/framework/comparador/Comparador.java
@@ -8,10 +8,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Comparador {
-    public static <T>List<Diferenca> comparador(T bg1, T bg2){
+    public static <T>List<Diferenca> comparador(T obj1, T obj2){
         List<Diferenca> diff = new ArrayList<>();
 
-        Class<?> clazz1 = bg1.getClass();
+        Class<?> clazz1 = obj1.getClass();
 
         for(Field field: clazz1.getDeclaredFields()){
             if(field.isAnnotationPresent(Ignorar.class)) {
@@ -21,8 +21,8 @@ public class Comparador {
                 Object valor2 = null;
                 try {
                     field.setAccessible(true);
-                    valor1 = field.get(bg1);
-                    valor2 = field.get(bg2);
+                    valor1 = field.get(obj1);
+                    valor2 = field.get(obj2);
                 } catch (IllegalAccessException e) {
                     throw new RuntimeException(e);
                 }
